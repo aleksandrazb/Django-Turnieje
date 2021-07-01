08a60374932a83eef3bd9188653ff404e1dc5782
aleksandrazb <ola.zbaska@wp.pl>
2021-07-01 23:16:08 +0200

    poprawka - autor turnieju na Anonymus

--- project_turnieje/app_turnieje/Models/turnieje_model.py
+++ project_turnieje/app_turnieje/Models/turnieje_model.py
@@ -9,7 +9,8 @@ class Turnieje(models.Model):
     ilosc_graczy = models.IntegerField(choices=[(1, '2'), (2, '4'), (3, '6'), (4, '8'), (5, '10'), (6, '12'), (7, '14'),
                                                 (8, '16'), (9, '18'), (10, '20'), (11, '22'), (12, '24')])
     data_rozpoczecia = models.DateTimeField()
-    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    autor = models.CharField(max_length=50, default="Anonymus")
 
     def get_absolute_url(self):
         return reverse('turniej_detail', kwargs={'pk': self.id})
