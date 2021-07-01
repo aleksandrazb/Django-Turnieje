a2cd602c5d43858d5fcbc38e71549da85cc8249c
aleksandrazb <58935989+aleksandrazb@users.noreply.github.com>
2021-07-01 23:40:43 +0200

    Views/mecze.py zrobione - autouzupełnianie pól (do sprawdzenia)

--- project_turnieje/app_turnieje/Views/mecze.py
+++ project_turnieje/app_turnieje/Views/mecze.py
@@ -18,18 +18,37 @@ def mecze_view(request, turniej_id):
     return render(request, 'szczegoly_turnieju.html', data)
 
 
-def edytuj_mecz_view(request, id_meczu):  # TODO: autouzupełnianie pól
+def edytuj_mecz_view(request, id_meczu):  # TODO: zapisywanie zmienionych danych
     print(request.user)
+    mecz = Mecze.objects.get(id=id_meczu)
+    init = {
+        'id_turnieju': mecz.id_turnieju,
+        'faza': mecz.faza,
+        'id_gracza1': mecz.id_gracza1,
+        'id_gracza2': mecz.id_gracza2,
+        'wynik_gracza1': mecz.wynik_gracza1,
+        'wynik_gracza2': mecz.wynik_gracza2,
+        'wygrana': mecz.wygrana
+    }
+    form = MeczeForm(initial=init)
+
     if request.POST:
-        mecz = Mecze.objects.get(id=id_meczu)
         form = MeczeForm(request.POST, instance=mecz)
         if form.is_valid():
             form.save()
 
-    data = {
-        'form': form,
-        'name': request.user,
-        'title': 'Edytuj mecz',
-        'edit': mecz
-    }
-    return render(request, 'edytuj_mecz.html', data)
+        data = {
+            'form': form,
+            'name': request.user,
+            'title': 'Edytuj mecz',
+            'edit': mecz
+        }
+        return render(request, 'edytuj_mecz.html', data)
+    else:
+        data = {
+            'form': form,
+            'name': request.user,
+            'title': 'Edytuj mecz',
+            'edit': mecz
+        }
+        return render(request, 'edytuj_mecz.html', data)
